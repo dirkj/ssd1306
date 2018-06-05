f1792f0224caa229dcf0e7dfdddd26a4eb3c2796
Dirk Jahnke <dirk.jahnke@mailbox.org>
2018-06-05 09:32:55 +0200

    Added font _fonts_terminus_bold_12x24_iso8859_1

--- src/fonts.c
+++ src/fonts.c
@@ -9,6 +9,7 @@ extern const font_info_t tahoma_8pt_font_info;
 extern const font_info_t _fonts_bitocra_4x7_ascii_info;
 extern const font_info_t _fonts_terminus_8x14_iso8859_1_info;
 extern const font_info_t _fonts_terminus_bold_8x14_iso8859_1_info;
+extern const font_info_t _fonts_terminus_bold_12x24_iso8859_1_info;
 
 const font_info_t * fonts[NUM_FONTS] =
 {
@@ -16,5 +17,6 @@ const font_info_t * fonts[NUM_FONTS] =
     &tahoma_8pt_font_info,
     &_fonts_bitocra_4x7_ascii_info,
     &_fonts_terminus_8x14_iso8859_1_info,
-    &_fonts_terminus_bold_8x14_iso8859_1_info
+    &_fonts_terminus_bold_8x14_iso8859_1_info,
+    &_fonts_terminus_bold_12x24_iso8859_1_info
 };
